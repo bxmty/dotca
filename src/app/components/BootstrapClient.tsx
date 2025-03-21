beb67d3b70eb58cab0267eecf5da2c08e486bff5
Matt Mattice <matticem@boximity.ca>
2025-03-20 21:31:20 -0400

    linting

--- src/app/components/BootstrapClient.tsx
+++ src/app/components/BootstrapClient.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function BootstrapClient() {
   useEffect(() => {
     // Dynamic import of Bootstrap JS on the client side only
-    import("bootstrap/dist/js/bootstrap.bundle.min.js" as any);
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
     
     // Also add a class to body when the component mounts
     // to make it easier to detect client-side rendering
