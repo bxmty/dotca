e5d4952ccee236918f672a98018a9a9f8f746a00
Matt Mattice <matticem@boximity.ca>
2025-03-25 20:05:11 -0400

    updated linting and brevo api key

--- src/types/jest.d.ts
+++ src/types/jest.d.ts
@@ -8,7 +8,7 @@ declare global {
   // Add types for expect, describe, it, etc. that are normally provided by @types/jest
   function describe(name: string, fn: () => void): void;
   function it(name: string, fn: () => void): void;
-  function expect(value: any): any;
+  function expect<T = unknown>(value: T): jest.Matchers<T>;
   function beforeAll(fn: () => void): void;
   function afterAll(fn: () => void): void;
   function beforeEach(fn: () => void): void;
